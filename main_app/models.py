80ca7d39f50558d7b6865a62d15a6e8c527c7c83
Lillian Chernin <l.smith1654@gmail.com>
2018-02-18 15:49:45 -0600

    update models

--- main_app/models.py
+++ main_app/models.py
@@ -5,37 +5,47 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     is_donor = models.BooleanField(default=False)
     is_org_admin = models.BooleanField(default=False)
+    email_address = models.EmailField()
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
 
 class Donor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    email_address = models.EmailField()
-    first_name = models.CharField()
-    last_name = models.CharField()
+    location = models.CharField(max_length=100)
+
+
+class OrgAdmin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    org_name = models.CharField(max_length=100)
+    org_location = models.CharField(max_length=100)
+    org_bio = models.CharField(max_length=300)
+
 
 class ReliefEffort(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=300)
+    desc = models.CharField(max_length=300)
     location = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
-    org_admin_id = models.ForeignKey(User,
+    org_admin_id = models.ForeignKey(OrgAdmin,
     models.SET_NULL,
     blank=True,
     null=True)
     def __str__(self):
 	    return self.name
 
-class Need(models.Model):
-    name = models.CharField(max_length=100)
-    desc = models.CharField(max_length=150)
+class ItemRequest(models.Model):
+    name = models.CharField(max_length=40)
+    desc = models.CharField(max_length=100)
+    is_fulfilled = models.BooleanField(default=False)
     relief_effort_id = models.ForeignKey(ReliefEffort,
     on_delete=models.CASCADE)
     def __str__(self):
         return self.name
 
 class Donation(models.Model):
-    need_id = models.ForeignKey(Need,
+    item_request_id = models.ForeignKey(ItemRequest,
     models.SET_NULL,
     blank=True,
     null=True)
